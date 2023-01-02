4c28acb31b354b1123841d6b5a6719757f3f4240
Nivetha.A.E <118973744+Nivetha-050302@users.noreply.github.com>
2023-01-02 11:43:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+let timer = 10;
+ let input_timer = document.getElementById('getthevalue') 
+ setTimeout(()=>{
+  input_timer.innerHTML = timer--;
+  input_timer.className = "timercountdown"
+  setTimeout(()=>{
+    input_timer.innerHTML = timer--;
+    setTimeout(()=>{
+      input_timer.innerHTML = timer--;
+      setTimeout(()=>{
+        input_timer.innerHTML = timer--;
+        setTimeout(()=>{
+          input_timer.innerHTML = timer--;
+          setTimeout(()=>{
+            input_timer.innerHTML = timer--;
+            setTimeout(()=>{
+              input_timer.innerHTML = timer--;
+              setTimeout(()=>{
+                input_timer.innerHTML = timer--;
+                setTimeout(()=>{
+                  input_timer.innerHTML = timer--;
+                  setTimeout(()=>{
+                    input_timer.innerHTML = timer--;
+                    setTimeout(()=>{
+                      input_timer.innerHTML = timer--;
+                      setTimeout(()=>{
+                        input_timer.style.color = 'red'
+                        input_timer.style.backgroundColor ="BLUE"
+                        input_timer.innerHTML = "HAPPY BIRTHDAY MOM ";
+                      
+                        
+                       },1000)
+                    
+                     },1000)
+                  
+                   },1000)
+                
+                 },1000)
+              
+               },1000)
+            
+             },1000)
+          
+           },1000)
+        
+         },1000)
+      
+       },1000)
+    
+     },1000)
+  
+   },1000)
+
+ },1000)
